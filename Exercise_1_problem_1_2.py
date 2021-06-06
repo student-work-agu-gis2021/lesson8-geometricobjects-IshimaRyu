f7319f018df47015e47a328e7bbad21b282a0b9a
IshimaRyu <>
2021-06-06 03:26:02 +0000

    I created a  function called create_point_geom.

--- Exercise_1_problem_1_2.py
+++ Exercise_1_problem_1_2.py
@@ -11,6 +11,9 @@
 
 from shapely.geometry import Point, LineString, Polygon
 #YOUR CODE HERE 1 to define create_point_geom()
+def create_point_geom(x_coord,y_coord):
+  point=Point(x_coord,y_coord)
+  return(point)
 
 # Test your function by running these code cells:
 
@@ -27,6 +30,7 @@ print(point1.geom_type)
 # 
 
 # YOUR CODE HERE 2 to define create_line_geom()
+
 
 # Demonstrate the usage of your function; For example, create a line object with two points: `Point(45.2, 22.34)` & `Point(100.22, -3.20)` and store the result in a variable called `line1`:
 
